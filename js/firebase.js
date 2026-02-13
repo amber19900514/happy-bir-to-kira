f736de94db306f143e669ce6a3f5d46cd664a18d
amber19900514 <amber19900514@gmail.com>
2026-02-13 12:25:14 +0900

    Revise Firebase config and import statements
    Updated Firebase configuration with new project details.

--- js/firebase.js
+++ js/firebase.js
@@ -10,9 +10,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "你的",
-  authDomain: "你的",
-  projectId: "你的",
+  apiKey: "AIzaSyC0B0ushQN85ttGRG4b3E45qg4eR5gM5VI",
+  authDomain: "happy-bir-to-kira.firebaseapp.com",
+  projectId: "happy-bir-to-kira",
+  storageBucket: "happy-bir-to-kira.firebasestorage.app",
+  messagingSenderId: "1035953659828",
+  appId: "1:1035953659828:web:49bd41e8f4d65d37cddce1",
+  measurementId: "G-GP1MPQ9EFC"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -34,3 +38,4 @@ export function listenWishes(callback) {
     callback(data);
   });
 }
+
